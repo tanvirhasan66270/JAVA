7e2baf4b2a994119ec25866be866a715ce05e557
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-01-25 17:51:13 +0600

    add

---
+++ NetBeansProjects/ComputerArea/src/main/java/com/mycompany/computerarea/ComputerArea.java
@@ -0,0 +1,61 @@
+//package com.mycompany.computerarea;
+//
+//public class ComputerArea {
+//
+//    public static void main(String[] args) {
+//        float radius;
+////        float area (exclusiv casting)
+//        double area; 
+////        (inclusiv custing)
+//        radius = 5;
+////        area = (float) 3.1416 * radius * radius;
+//        area = 3.1416 * radius * radius;
+//        System.out.println("Area of a circle is" + area);
+//
+//    }
+//}
+package com.mycompany.computerarea;
+
+import java.util.Scanner;
+
+//public class ComputerArea {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner input = new Scanner(System.in);
+//        float area;
+//        System.out.println("Inter Number");
+//        float radius = input.nextFloat();
+//
+//        area = (float) .1416 * radius * radius;
+//        System.out.println("Area of a circle is\" + area);
+//
+//    }
+//}
+
+//public class ComputerArea {
+//
+//    public static void main(String[] args) {
+//
+//        Scanner input = new Scanner(System.in);
+//       String name;
+//        System.out.println("Inter your Name ");
+//         name = input.nextLine();
+//
+//        
+//        System.out.println("My Name is " +name );
+//
+//    }
+//}
+
+//public class ComputerArea{
+//    public static void main(String[] args) {
+//        Scanner InputName= new Scanner(System.in);
+//        String name;
+//        System.out.println("Inter Name");
+//        name=InputName.nextLine();
+//        System.out.println("My name is "+name);
+//    }
+//}
+
+
---
+++ NetBeansProjects/DirstJavaProject/src/dirstjavaproject/FirstJavaProject.java
@@ -0,0 +1,9 @@
+package dirstjavaproject;
+
+public class FirstJavaProject {
+
+    public static void main(String[] args) {
+
+    }
+
+}
