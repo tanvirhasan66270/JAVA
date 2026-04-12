fa23daee7bc2b6b5044c13569452079339ccd38a
Md. Tanvir <tanvirhasan66270@gmail.com>
2026-04-12 14:23:41 +0600

    add

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -2,19 +2,20 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package evdexam;
+package javaapplication8;
 
 /**
  *
  * @author Admin
  */
-public class EvdExam {
+public class JavaApplication8 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("");
     }
     
 }
